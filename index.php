a39086c4e51bf37a424c719b624a65ab4aec67dd
Lorenzo Del Re <136103234+LoreWebDev24@users.noreply.github.com>
2023-11-28 14:48:21 +0100

    add the basic struct.

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php 
 
 class Production {
-    // public $title;
-    // public $language;
-    // public $rating;
+    public $title;
+    public $language;
+    public $rating;
+
+    function __construct($_title, $_language, $_rating) {
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->rating = $_rating;
+    }
 }
+
+$titanic = new Production('Titanic', 'ENGL', 9);
+$sharknado = new Production('Sharknado', 'ENGL', 4);
+$sherlockHolmes = new Production('Sherlock Holmes: Game of Shadows', 'ENGL', 7);
+
+$films = [$titanic,$sharknado,$sherlockHolmes];
+
+foreach ($films as $film ) {
+    var_dump($film);
+    echo $film->title . ' ';
+    echo $film->language . ' ';
+    echo $film->rating . ' ';
+}
+
+
+?>

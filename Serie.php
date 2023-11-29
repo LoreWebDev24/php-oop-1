cf9b02138c991765b04f564ca1d559837625e428
Lorenzo Del Re <136103234+LoreWebDev24@users.noreply.github.com>
2023-11-29 15:36:31 +0100

    add Serie.php that ext. Production class

---
+++ Serie.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . './Production.php';
+
+class Serie extends Production {
+    
+    protected $season;
+
+    function __construct(String $_title, String $_language, $_rating, $_season) {
+  
+      parent::__construct($_title, $_language, $_rating);
+      $this->setSeason($_season);
+    }
+
+    public function setSeason($_season){
+        $this->season = $_season;
+    }
+
+    public function getSeason() {
+        return $this->season;
+    }
+
+}
